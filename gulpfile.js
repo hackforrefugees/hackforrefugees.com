65dc9b3a18ab7774c9b01f44c2edb8e8bb856645
Pierre Reimertz <pierre@pierrereimertz.com>
2015-11-10 06:13:38 +0100

    version hackz0r-i-feel-bad-about-every-line-i-have-written-but -i-feel-very-good-about-the-beautiful-design-malin-made

--- gulpfile.js
+++ gulpfile.js
@@ -59,13 +59,6 @@ gulp.task('html', ['templates', 'stylesheets'], function () {
 
 gulp.task('images', function () {
   return gulp.src('src/images/**/*')
-    // .pipe($.cache($.imagemin({
-    //   progressive: true,
-    //   interlaced: true,
-    //   // don't remove IDs from SVGs, they are often used
-    //   // as hooks for embedding and styling
-    //   svgoPlugins: [{cleanupIDs: false}]
-    // })))
     .pipe(gulp.dest('.dist/images'));
 });
 
